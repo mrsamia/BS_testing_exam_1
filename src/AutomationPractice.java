4a81f8723c3c17e97dba4aeb0b9f66c2d1e04d49
mrsamia <15203060@iubat.edu>
2021-04-03 19:36:29 +0600

    SRM | driver config done

--- src/AutomationPractice.java
+++ src/AutomationPractice.java
@@ -5,7 +5,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class AutomationPractice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Downloads\\Compressed\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://automationpractice.com/index.php");
 
 	}
 
