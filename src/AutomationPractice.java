e24323c0dd99f54e86ab9b1eee19221fa5b0a0a7
mrsamia <15203060@iubat.edu>
2021-04-03 21:57:55 +0600

    SRM | gender selection, firstname,lastname

--- src/AutomationPractice.java
+++ src/AutomationPractice.java
@@ -1,6 +1,8 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class AutomationPractice {
@@ -20,7 +22,19 @@ public class AutomationPractice {
 		
 		driver.findElement(By.id("SubmitCreate")).click();
 		System.out.println("Account create successfully done");
-
+		
+		
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("id_gender2")));
+		
+		driver.findElement(By.id("id_gender2")).click();
+		System.out.println("Gender Seelction done");
+		
+		driver.findElement(By.id("customer_firstname")).sendKeys("Samia");
+		System.out.println("First name typed");
+		
+		driver.findElement(By.id("customer_lastname")).sendKeys("Rahman");
+		System.out.println("last name typed");
 	}
 
 }
